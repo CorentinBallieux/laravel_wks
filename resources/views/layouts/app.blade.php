19ce2b14ac1708eaf88c6a1bbcbb48470aff205f
CorentinBallieux <31621284+CorentinBallieux@users.noreply.github.com>
2026-05-21 21:44:43 +0200

    fin du cours

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Laravel')</title>
+    <title>@yield('title', 'Titre par defaut')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50 min-h-screen text-gray-800 antialiased">
